49a8ecd57b0fe898c8adbfa8f255f0407da1acaf
niklas.therning <niklas.therning@a44ee54a-9095-11dd-919c-2bbf662b16d1>
2009-03-23 09:10:08 +0000

    Added DisableSessionIdInUrlFilter

---
+++ trunk/src/main/java/se/trillian/goodies/web/DisableSessionIdInUrlFilter.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2004-2009, Trillian AB. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trillian.goodies.web;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+/**
+ * {@link Filter} implementation which prevents the session id from being
+ * encoded in the URL. 
+ *
+ * @author Niklas Therning
+ * @version $Id$
+ */
+public class DisableSessionIdInUrlFilter implements Filter {
+    private static final String SESSION_ID_REG_EXP = "(?i);jsessionid=[0-9a-z]+";
+    private static final Pattern SESSION_ID_PATTERN = 
+        Pattern.compile(SESSION_ID_REG_EXP);
+
+    public void doFilter(ServletRequest req, ServletResponse res,
+                         FilterChain chain) 
+            throws IOException, ServletException {
+        
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        
+        HttpServletRequest newRequest = new RequestWrapper(request);
+        HttpServletResponse newResponse = new ResponseWrapper(response);
+        
+        chain.doFilter(newRequest, newResponse);
+    }
+
+    public void init(FilterConfig fc) throws ServletException {}
+    public void destroy() {}
+    
+    private static class RequestWrapper extends HttpServletRequestWrapper {
+        private String requestUri = null;
+        private String requestUrl = null;
+        
+        public RequestWrapper(HttpServletRequest request) {
+            super(request);
+        }
+        public boolean isRequestedSessionIdFromURL() {
+            return false;
+        }
+        public boolean isRequestedSessionIdFromUrl() {
+            return false;
+        }
+        public StringBuffer getRequestURL() {
+            if (requestUrl == null) {
+                requestUrl = removeSessionId(super.getRequestURL().toString());
+            }
+            return new StringBuffer(requestUrl);
+        }
+        public String getRequestURI() {
+            if (requestUri == null) {
+                requestUri = removeSessionId(super.getRequestURI());
+            }
+            return requestUri;
+        }
+        private String removeSessionId(String s) {
+            return SESSION_ID_PATTERN.matcher(s).replaceFirst("");
+        }
+    }
+    
+    private static class ResponseWrapper extends HttpServletResponseWrapper {
+
+        public ResponseWrapper(HttpServletResponse response) {
+            super(response);
+        }
+        public String encodeRedirectUrl(String url) {
+            return url;
+        }
+        public String encodeRedirectURL(String url) {
+            return url;
+        }
+        public String encodeUrl(String url) {
+            return url;
+        }
+        public String encodeURL(String url) {
+            return url;
+        }
+    }
+    
+}
