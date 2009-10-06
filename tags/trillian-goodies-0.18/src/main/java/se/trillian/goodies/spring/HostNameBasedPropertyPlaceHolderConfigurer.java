d29fcadeae1ee4e3b596077c8fe0303c75b2fbd0
niklas.therning <niklas.therning@a44ee54a-9095-11dd-919c-2bbf662b16d1>
2009-10-06 06:48:47 +0000

    [maven-scm] copy for tag trillian-goodies-0.18

---
+++ tags/trillian-goodies-0.18/src/main/java/se/trillian/goodies/spring/HostNameBasedPropertyPlaceHolderConfigurer.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright (c) 2004-2008, Trillian AB. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trillian.goodies.spring;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+/**
+ * <p>
+ * Spring {@link PropertyPlaceholderConfigurer} which reads host name specific
+ * properties files. The names of the files will be derived from the location(s)
+ * set using {@link #setLocations(Resource[])}. E.g. if a single resource,
+ * <code>file:/some/path/jdbc.props</code>, has been set the following resources
+ * will be used in the listed order:
+ * <ol>
+ *      <li>file:/some/path/jdbc-defaults.props</li>
+ *      <li>file:/some/path/jdbc-defaults-<i>&lt;hostname&gt;</i>.props</li>
+ *      <li>file:/some/path/jdbc.props</li>
+ *      <li>file:/some/path/jdbc-<i>&lt;hostname&gt;</i>.props</li>
+ * </ol>
+ * </p>
+ * <p>
+ * The <code>hostNameFilterRegex</code> and <code>hostNameFilterReplacement</code> 
+ * properties can be used to transform the host name before the resource 
+ * locations are determined. E.g. if the host name is <code>test-server1</code>,
+ * <code>hostNameFilterRegex</code> has been set to the regular expression 
+ * <code>([\w-.]+)\d*</code> and <code>hostNameFilterReplacement</code> has been 
+ * set to <code>$1</code> the following resources will be loaded:
+ * <ol>
+ *      <li>file:/some/path/jdbc-defaults.props</li>
+ *      <li>file:/some/path/jdbc-defaults-test-server.props</li>
+ *      <li>file:/some/path/jdbc-defaults-test-server1.props</li>
+ *      <li>file:/some/path/jdbc.props</li>
+ *      <li>file:/some/path/jdbc-test-server.props</li>
+ *      <li>file:/some/path/jdbc-test-server1.props</li>
+ * </ol>
+ * </p>
+ * <p>
+ * If multiple locations have been set they will be expanded in the same manner
+ * and loaded in the order they come in the array of locations passed to
+ * {@link #setLocations(Resource[])}.
+ * </p>
+ * <p>
+ * If the same property occurs in more than one of the files the one loaded 
+ * last will be used. If any of the files listed above is missing it will be 
+ * skipped (unless {@link #setIgnoreResourceNotFound(boolean)} has been
+ * set to <code>false</code>).
+ * </p>
+ * <p>
+ *   The default location is <code>classpath:/spring/spring.properties</code>.
+ * </p>
+ * 
+ * @author Niklas Therning
+ * @version $Id$
+ */
+public class HostNameBasedPropertyPlaceHolderConfigurer extends
+                PropertyPlaceholderConfigurer {
+
+    private static final Log log = LogFactory.getLog(HostNameBasedPropertyPlaceHolderConfigurer.class);
+    
+    private String hostNameFilterRegex = null;
+    private String hostNameFilterReplacement = null;
+    private Resource[] locations;
+    
+    public HostNameBasedPropertyPlaceHolderConfigurer() {
+        setLocation(new ClassPathResource("/spring/spring.properties"));
+        setIgnoreResourceNotFound(true);
+    }
+    
+    public void setHostNameFilterRegex(String hostNameFilterRegex) {
+        this.hostNameFilterRegex = hostNameFilterRegex;
+    }
+    
+    public void setHostNameFilterReplacement(String hostNameFilterReplacement) {
+        this.hostNameFilterReplacement = hostNameFilterReplacement;
+    }
+    
+    @Override
+    public void setLocation(Resource location) {
+        this.setLocations(new Resource[] {location});
+    }
+    
+    @Override
+    public void postProcessBeanFactory(
+            ConfigurableListableBeanFactory beanFactory) throws BeansException {
+
+
+        String hostName = "localhost";
+        try {
+            hostName = getHostName();
+        } catch (UnknownHostException uhe) {
+            log.warn("Could not determine the name for the current host. Using '" 
+                    + hostName + "'.");
+        }
+
+        String filteredHostName = null;
+        if (hostNameFilterRegex != null && hostNameFilterReplacement != null) {
+            filteredHostName = hostName.replaceAll(hostNameFilterRegex, hostNameFilterReplacement);
+        }
+        
+        try {
+            
+            List<Resource> newLocations = new ArrayList<Resource>();
+            for (Resource res : locations) {
+                String basename = res.getFilename();
+                String extension = "";
+                int index = basename.lastIndexOf('.');
+                if (index != -1) {
+                    extension = basename.substring(index + 1);
+                    basename = basename.substring(0, index);
+                }
+                newLocations.add(res.createRelative(basename + "-defaults." + extension));
+                if (filteredHostName != null) {
+                    newLocations.add(res.createRelative(basename + "-defaults-" + filteredHostName +  "." + extension));
+                }
+                newLocations.add(res.createRelative(basename + "-defaults-" + hostName +  "." + extension));
+                newLocations.add(res.createRelative(basename + "." + extension));
+                if (filteredHostName != null) {
+                    newLocations.add(res.createRelative(basename + "-" + filteredHostName +  "." + extension));
+                }
+                newLocations.add(res.createRelative(basename + "-" + hostName + "." + extension));
+            }
+            
+            super.setLocations(newLocations.toArray(new Resource[0]));
+            
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }        
+        
+        super.postProcessBeanFactory(beanFactory);
+    }
+    
+    @Override
+    public void setLocations(Resource[] locations) {
+        this.locations = locations;
+    }
+    
+    protected String getHostName() throws UnknownHostException {
+        return InetAddress.getLocalHost().getHostName();
+    }
+    
+}
---
+++ tags/trillian-goodies-0.18/src/main/java/se/trillian/goodies/stripes/ViewForwardResolution.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright (c) 2004-2008, Trillian AB. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.trillian.goodies.stripes;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sourceforge.stripes.action.ActionBean;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.Resolution;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.util.Assert;
+
+
+/**
+ * Stripes {@link Resolution} which forwards to a view. If the no args constructor
+ * is used the view name will be derived from the URL of the current request.
+ * 
+ * <p>
+ * Based on the view name, a JSP file will resolved according to 
+ * <code>
+ *   getPrefix() + viewName + getSuffix()
+ * </code>
+ * The default prefix is <code>/WEB-INF/jsp</code> and the default suffix is
+ * <code>.jsp</code>.
+ * </p>
+ * <p>If no view name is specified, the view will be resolved using 
+ * the protected method {@link #extractViewName(String, HttpServletRequest)}.
+ * The default behaviour is to extract the view name from the request uri by first
+ * removing the context path ({@link HttpServletRequest#getContextPath()} and then
+ * apply the view name suffix pattern that can be set using 
+ * {@link #setViewNameSuffixPattern(String)}. The default pattern is
+ * <code>(_[\\w]+)?\\.[\\w]+$</code>. 
+ * </p>
+ * <p>
+ * Examples of the default behaviour:
+ * <ul>
+ *   <li>http://www.host.com/theapp/subfolder/index.html -&gt; /subfolder/index -&gt; /WEB-INF/jsp/subfolder/index.jsp</li>
+ *   <li>http://www.host.com/theapp/subfolder/abc_en.html -&gt; /subfolder/abc -&gt; /WEB-INF/jsp/subfolder/abc.jsp</li>
+ *   <li>http://www.host.com/theapp/folder/subfolder/DoSomething.action -&gt; /folder/subfolder/DoSomething -&gt; /WEB-INF/jsp/folder/subfolder/DoSomething.jsp</li>
+ * </ul>
+ * </p>
+ *
+ * @author Henric Müller
+ * @author NiklasTherning
+ * @version $Id$
+ */
+public class ViewForwardResolution extends ForwardResolution {
+    private static final Log log = LogFactory.getLog(ViewForwardResolution.class);
+    
+    private String prefix = "/WEB-INF/jsp";
+    private String suffix = ".jsp";
+    private String viewNameSuffixPattern = "(_[\\w]+)?\\.[\\w]+$";
+    
+    private boolean checkExists = false;
+    
+    /**
+     * Constructs a new instance which forwards to the JSP corresponding to 
+     * the current request's URL.
+     */
+    public ViewForwardResolution() {
+        super((String) null);
+    }
+    
+    /**
+     * Constructs a new instance which forwards to the JSP corresponding to the 
+     * specified view.
+     *  
+     * @param viewName the name of the view. The prefix set with {@link #setPrefix(String)} 
+     *               will be prepended and the suffix set with {@link #setSuffix(String)} will be 
+     *               appended to get the final name of the JSP.
+     */
+    public ViewForwardResolution(String viewName) {
+        super("");
+        if (!viewName.startsWith("/")) {
+            viewName = "/" + viewName;
+        }
+        setPath(prefix + viewName + suffix);
+    }
+
+    public ViewForwardResolution(Class<? extends ActionBean> beanType) {
+        super(beanType);
+    }
+
+    public ViewForwardResolution(Class<? extends ActionBean> beanType, String event) {
+        super(beanType, event);
+    }
+    
+    protected String extractViewName(String uri, HttpServletRequest request) {
+        String contextPath = request.getContextPath();
+        uri = StringUtils.removeStart(uri, contextPath);
+        return uri.replaceFirst(viewNameSuffixPattern, "");
+        
+    }
+    
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (this.getPath() == null) {
+            String viewName = extractViewName(request.getRequestURI(), request);
+            setPath(prefix + viewName + suffix);
+        }
+        
+        if (checkExists) {
+            /*
+             * Check if the JSP actually exists. Without this code the SAXWriter
+             * will throw an exception if the JSP doesn't exist and we'll get a 500
+             * error instead.
+             */
+            String realPath = request.getSession().getServletContext().getRealPath(getPath());
+            if (!new File(realPath).exists()) {
+                log.warn("View " + getPath() + " not found");
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+                return;
+            }
+        }
+        super.execute(request, response);
+    }
+
+    /**
+     * If set to <code>true</code> this class will check if the view JSP 
+     * actually exists before forwarding to it. If not set the {@link SAXWriter}
+     * will throw an exception if the JSP doesn't exist and we'll get a 500
+     * error instead of the more appropriate 404 error. This should only be
+     * set for the default {@link ActionBean} (like {@link DefaultActionBean})
+     * which handles URLs which haven't been mapped to an explicit 
+     * {@link ActionBean}. 
+     */
+    public ViewForwardResolution checkIfExists(boolean b) {
+        this.checkExists = b;
+        return this;
+    }
+    
+    public void setPrefix(String prefix) {
+        Assert.notNull(prefix);
+        this.prefix = prefix;
+    }
+    
+    public void setSuffix(String suffix) {
+        Assert.notNull(suffix);
+        this.suffix = suffix;
+    }
+    
+    public void setViewNameSuffixPattern(String viewNameSuffixPattern) {
+        this.viewNameSuffixPattern = viewNameSuffixPattern;
+    }
+    
+}
