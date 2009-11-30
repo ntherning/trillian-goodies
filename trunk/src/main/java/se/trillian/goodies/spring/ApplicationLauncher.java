c5aa11d21d3d226f19ffe0fa8cf989bf38c4c6a9
niklas.therning <niklas.therning@a44ee54a-9095-11dd-919c-2bbf662b16d1>
2009-11-30 13:34:14 +0000

    Removed all code which uses Log4j. Changed ApplicationLauncher to use Logback instead of Log4j.

--- trunk/src/main/java/se/trillian/goodies/spring/ApplicationLauncher.java
+++ trunk/src/main/java/se/trillian/goodies/spring/ApplicationLauncher.java
@@ -21,15 +21,18 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.PropertyConfigurator;
-import org.apache.log4j.extras.DOMConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.AbstractXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.tanukisoftware.wrapper.WrapperListener;
 import org.tanukisoftware.wrapper.WrapperManager;
+
+import ch.qos.logback.classic.BasicConfigurator;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.util.StatusPrinter;
 
 /**
  * Launches a standalone (not running in a web container) application by loading
@@ -38,36 +41,21 @@ import org.tanukisoftware.wrapper.WrapperManager;
  * files may be located in the classpath if they are prefixed with the 
  * <code>classptah*:</code> prefix. Execute it like this:
  * <pre>
- *      ApplicationLauncher [[-d log-file-reload-delay-in-secs] log4j-properties-file]
- *                                    [context1.xml [context2.xml ...]]
+ *      ApplicationLauncher [logback-config-file] [context1.xml [context2.xml ...]]
  * </pre>
  * <p>
- * If no Log4j configuration file is specified this class will first look for a 
- * file named <code>log4j-<i>&lt;hostname&gt;</i>.(properties|xml)</code> in the 
+ * If no Logback configuration file is specified this class will first look for a 
+ * file named <code>logback-<i>&lt;hostname&gt;</i>.xml</code> in the 
  * working directory. If no such file is found it will look for a file named 
- * <code>log4j-<i>&lt;hostname-prefix&gt;</i>.(properties|xml)</code> where 
+ * <code>logback-<i>&lt;hostname-prefix&gt;</i>.xml</code> where 
  * <i>hostname-prefix</i> is a prefix of the host name. The file with the 
  * longest prefix of the host name in its name will be loaded.
  * </p>
  * <p>
  * If no file is found containing the host name or a prefix of it the file named
- * <code>log4j.(properties|xml)</code> will be used. If no such file exists Log4j will 
+ * <code>logback.xml</code> will be used. If no such file exists Logback will 
  * be initialized using {@link BasicConfigurator}.
  * </p>
- * <p>
- * XML-based Log4j files will be loaded using the {@link DOMConfigurator}
- * class provided by the 
- * <a href="http://logging.apache.org/log4j/companions/extras/">Apache Log4j Extras project</a>.
- * The Extras project provides additional appenders, filters, formatters and 
- * other components for log4j 1.2.
- * </p>
- * <p>
- * Please note that if a Log4j file is specified it must have either the 
- * <code>.properties</code> or <code>.xml</code> file extension.
- * </p>
- * <p>
- * If no reload time has been given 5 seconds will be used. Use 0 to disable 
- * reloading completely.
  * <p>
  * If no Spring context files have been specified this class will load all 
  * Spring files matching <code>classpath*:/spring/*.xml</code>.
@@ -86,16 +74,15 @@ public class ApplicationLauncher {
             System.err.println(msg);
         }
         System.err.println("Usage:");
-        System.err.println("ApplicationLauncher [[-d log-file-reload-delay-in-secs] "
-                        + "<log4j-properties-file>] [context1.xml [context2.xml ...]] ...");
+        System.err.println("ApplicationLauncher [logback-config-file] [context1.xml [context2.xml ...]] ...");
     }
     
     public static final void main(String[] args) throws Throwable {
         /*
          * Use BasicConfigurator temporarily to enable logging before the 
-         * log4j.properties file has been read
+         * logback file has been read
          */
-        BasicConfigurator.configure();
+        BasicConfigurator.configureDefaultContext();
         
         boolean useWrapper = false;
         try {
@@ -116,17 +103,9 @@ public class ApplicationLauncher {
     public Integer start(String[] args) {
         String logFile = null;
         ArrayList<String> springFiles = new ArrayList<String>();
-        long delay = 5;
         try {
             int i = 0;
-            if (i < args.length && args[i].equals("-d")) {
-                i++;
-                delay = Long.parseLong(args[i++]);
-                if (delay < 0) {
-                    throw new IllegalArgumentException("Specified delay < 0");
-                }
-            }
-            if (i < args.length && args[i].toLowerCase().matches(".*\\.(properties|xml)")) {
+            if (i < args.length && args[i].toLowerCase().matches("logback.*\\.xml")) {
                 logFile = args[i++];
             }
             
@@ -139,33 +118,28 @@ public class ApplicationLauncher {
         }
         
         if (logFile == null) {
-            logFile = getDefaultLog4jFile();
+            logFile = getDefaultLogbackFile();
         }
         
         try {
             /*
-             * Initialize the log4j logging system.
+             * Initialize the logback logging system.
              */
             if (logFile != null) {
-                log.info("Configuring log4j from file '" + logFile + "'");
-                if (delay > 0) {
-                    log.info("Using a reload interval of " + delay + " second(s)");
-                    if (logFile.toLowerCase().endsWith(".xml")) {
-                        DOMConfigurator.configureAndWatch(logFile, delay * 1000);
-                    } else {
-                        PropertyConfigurator.configureAndWatch(logFile, delay * 1000);
-                    }
-                } else {
-                    log.info("Log4j will not reload the file if changed");
-                    if (logFile.toLowerCase().endsWith(".xml")) {
-                        DOMConfigurator.configure(logFile);
-                    } else {
-                        PropertyConfigurator.configure(logFile);
-                    }
-                }
+                log.info("Configuring logback from file '" + logFile + "'");
+                LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+                
+                try {
+                   JoranConfigurator configurator = new JoranConfigurator();
+                   configurator.setContext(lc);
+                   lc.reset();
+                   configurator.doConfigure(logFile);
+                } catch (JoranException je) {
+                   StatusPrinter.print(lc);
+                } 
             }
         } catch (Throwable t) {
-            log.error("Failed to initialize log4j logging system: " + t.getMessage(), t);
+            log.error("Failed to initialize logback logging system: " + t.getMessage(), t);
             return new Integer(2);
         }
             
@@ -178,7 +152,6 @@ public class ApplicationLauncher {
             }
             log.info("Loading spring files: " + springFiles);
             context = new FileSystemXmlApplicationContext(springFiles.toArray(new String[0]), false);
-            context.addBeanFactoryPostProcessor(new HostNameBasedPropertyPlaceHolderConfigurer());
             context.refresh();
         } catch (Throwable t) {
             log.error("Unable to load Spring configuration files", t);
@@ -188,7 +161,7 @@ public class ApplicationLauncher {
         return null;
     }
 
-    private String getLog4jFile(String path) throws FileNotFoundException {
+    private String getLogbackFile(String path) throws FileNotFoundException {
         File f = new File(path);
         if (f.isFile() && f.exists()) {
             return f.getAbsolutePath();
@@ -196,7 +169,7 @@ public class ApplicationLauncher {
         throw new FileNotFoundException();
     }
     
-    private String getDefaultLog4jFile() {
+    private String getDefaultLogbackFile() {
         String hostName = "localhost";
         try {
             hostName = InetAddress.getLocalHost().getHostName();
@@ -208,19 +181,13 @@ public class ApplicationLauncher {
         String prefix = hostName;
         while (prefix.length() > 0) {
             try {
-                return getLog4jFile("log4j-" + prefix + ".properties");
-            } catch (FileNotFoundException e) {}
-            try {
-                return getLog4jFile("log4j-" + prefix + ".xml");
+                return getLogbackFile("logback-" + prefix + ".xml");
             } catch (FileNotFoundException e) {}
             prefix = hostName.substring(0, prefix.length() - 1);
         }
 
         try {
-            return getLog4jFile("log4j.properties");
-        } catch (FileNotFoundException e) {}
-        try {
-            return getLog4jFile("log4j.xml");
+            return getLogbackFile("logback.xml");
         } catch (FileNotFoundException e) {}
 
         return null;
